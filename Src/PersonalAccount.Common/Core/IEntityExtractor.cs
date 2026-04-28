1185d781c3b2e7bd5e5ed9bc54aba06e6c3fe290
AItEKS <egorka.ku06@mail.ru>
2026-04-28 18:47:29 +0800

    fix

--- Src/PersonalAccount.Common/Core/IEntityExtractor.cs
+++ Src/PersonalAccount.Common/Core/IEntityExtractor.cs
@@ -3,14 +3,26 @@ using PersonalAccount.Domain.Models.Dto;
 
 namespace PersonalAccount.Common.Core;
 
+/// <summary>
+/// Интерфейс сервиса для выделения новых бизнес-сущностей из потока сырых данных.
+/// </summary>
 public interface IEntityExtractor
 {
-    // Находит категории, которых нет в справочнике
+    /// <summary>
+    /// Извлекает из набора транзакций категории номенклатуры, которых еще нет в базе данных.
+    /// </summary>
+    /// <param name="rows">Набор сырых строк журнала.</param>
     Task<IEnumerable<CategoryModel>> ExtractNewCategoriesAsync(IEnumerable<JournalRowDto> rows, CancellationToken token);
-    
-    // Находит товары, которых нет в справочнике
+
+    /// <summary>
+    /// Извлекает из набора транзакций позиции номенклатуры (товары), которых еще нет в базе данных.
+    /// </summary>
+    /// <param name="rows">Набор сырых строк журнала.</param>
     Task<IEnumerable<NomenclatureModel>> ExtractNewNomenclatureAsync(IEnumerable<JournalRowDto> rows, CancellationToken token);
-    
-    // Находит сотрудников, которых нет в справочнике
+
+    /// <summary>
+    /// Извлекает из набора транзакций сотрудников, информация о которых отсутствует в системе.
+    /// </summary>
+    /// <param name="rows">Набор сырых строк журнала.</param>
     Task<IEnumerable<EmploeeModel>> ExtractNewEmployeesAsync(IEnumerable<JournalRowDto> rows, CancellationToken token);
 }
--- Src/PersonalAccount.Common/Core/IJournalDataSource.cs
+++ Src/PersonalAccount.Common/Core/IJournalDataSource.cs
@@ -1,9 +1,17 @@
+using PersonalAccount.Domain.Models;
 using PersonalAccount.Domain.Models.Dto;
 
 namespace PersonalAccount.Common.Core;
 
+/// <summary>
+/// Интерфейс источника данных журнала. 
+/// </summary>
 public interface IJournalDataSource
 {
-    // Получает набор новых строк
-    Task<IEnumerable<JournalRowDto>> GetUnprocessedRowsAsync(int batchSize, CancellationToken token);
+    /// <summary>
+    /// Извлекает набор новых (необработанных) строк журнала на основе настроек загрузки.
+    /// </summary>
+    /// <param name="branch">Модель филиала, для которого запрашиваются данные.</param>
+    /// <param name="batchSize">Количество записей, извлекаемых за один вызов.</param>
+    Task<IEnumerable<JournalRowDto>> GetUnprocessedRowsAsync(BranchModel branch, int batchSize, CancellationToken token);
 }
